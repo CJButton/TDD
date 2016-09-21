92e38812a45c290c2f99552d4ff911385a114aa0
App Academy Student <student@appacademy.io>
2016-09-21 10:36:00 -0700

    Initial commit

---
+++ spec/tdd_spec.rb
@@ -0,0 +1,23 @@
+require 'tdd'
+require 'rspec'
+
+describe Array do
+
+  describe "#my_uniq" do
+    let(:an_array) { [1,2,1,3,3] }
+
+    it "should not modify the original array" do
+      an_array.my_uniq
+      expect(an_array).to eq([1,2,1,3,3])
+    end
+
+    it "should return only unique elements" do
+      expect(an_array.my_uniq).to eq(:an_array.uniq)
+    end
+
+    it "should not call #uniq" do
+      expect(an_array.my_uniq).to not_receive(:uniq)
+    end
+
+  end
+end
